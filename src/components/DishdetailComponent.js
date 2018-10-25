3bed6c00c72bbe5f6d50350571bd369725ecf0a6
nobuhiroK <nobuhk@uw.edu>
2018-10-25 18:15:23 +0900

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
-    function RenderComments({dish}){
-        if (dish){
-            const commentsList = dish.comments.map(comment => {
+
+
+    function RenderComments({comments}){
+        if (comments){
+            const commentsList = comments.map(comment => {
             return (
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
@@ -20,7 +24,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
             )
         })
             return (
-                <div className='col-12 col-md-5 m-1'>
+                <div>
                     <h4> Comments </h4>
                     <ul className='list-unstyled'>
                         {commentsList}
@@ -50,7 +54,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
         if (dish) {
 
             return(
-                <div  className="col-12 col-md-5 m-1">
+                // <div  className="col-12 col-md-5 m-1">
                     <Card>
                         <CardImg top src={dish.image} alt={dish.name} />
                             <CardBody>
@@ -58,7 +62,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
                                 <CardText>{dish.description}</CardText>
                             </CardBody>
                         </Card>
-                </div>
+                // </div>
                     );
 
                 } else{
@@ -75,18 +79,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
     const  DishDetail = (props) => {
         
         return (
-            <div className='container'>
-                <div className="row">    
-                    <RenderDish dish={props.dish} />
-                    <RenderComments dish={props.dish} />
-                    
-                    
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
 
-                    
-                    
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+            
     }
 
 export default DishDetail;
