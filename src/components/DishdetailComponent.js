e111149acfa478bc4fc39b509ca15b5aa2dd39e5
nobuhiroK <nobuhk@uw.edu>
2018-11-04 18:08:36 +0900

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if (comments){
             const commentsList = comments.map(comment => {
             return (
@@ -30,7 +30,7 @@ import { Link } from 'react-router-dom';
                     <h4> Comments </h4>
                     <ul className='list-unstyled'>
                         {commentsList}
-                        <CommentForm />
+                        <CommentForm dishId={dishId} addComment={addComment}/>
                     </ul>
                 </div>
 
@@ -99,7 +99,9 @@ import { Link } from 'react-router-dom';
                             <RenderDish dish={props.dish} />
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.comments} />
+                            <RenderComments comments={props.comments} 
+                            addComment={props.addComment} 
+                            dishId={props.dish.id}/>
                         </div>
                     </div>
                 </div>
@@ -132,9 +134,10 @@ class CommentForm extends Component{
       }
     
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
-        // event.preventDefault();
+        //console.log('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+
     }
     render(){
 
