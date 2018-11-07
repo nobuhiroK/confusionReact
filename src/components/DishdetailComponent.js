78494d2e16cfec7f4a7b9b5d179527d2ab015e3b
nobuhiroK <nobuhk@uw.edu>
2018-11-07 19:12:19 +0900

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 
@@ -59,7 +59,7 @@ import { Loading } from './LoadingComponent';
         return(
                 // <div  className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                             <CardBody>
                                 <CardTitle>{dish.name}</CardTitle>
                                 <CardText>{dish.description}</CardText>
