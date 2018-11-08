db57980e86a03d551a3ffe96968b04155ff686bc
nobuhiroK <nobuhk@uw.edu>
2018-11-08 19:25:59 +0900

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,7 +10,7 @@ import { baseUrl } from '../shared/baseUrl';
 
 
 
-    function RenderComments({comments, addComment, dishId}){
+    function RenderComments({comments, postComment, dishId}){
         if (comments){
             const commentsList = comments.map(comment => {
             return (
@@ -31,7 +31,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <h4> Comments </h4>
                     <ul className='list-unstyled'>
                         {commentsList}
-                        <CommentForm dishId={dishId} addComment={addComment}/>
+                        <CommentForm dishId={dishId} postComment={postComment}/>
                     </ul>
                 </div>
 
@@ -118,7 +118,7 @@ import { baseUrl } from '../shared/baseUrl';
                             </div>
                             <div className="col-12 col-md-5 m-1">
                                 <RenderComments comments={props.comments} 
-                                addComment={props.addComment} 
+                                 postComment={props.postComment}
                                 dishId={props.dish.id}/>
                             </div>
                         </div>
@@ -155,7 +155,7 @@ class CommentForm extends Component{
     handleSubmit(values) {
         //console.log('Current State is: ' + JSON.stringify(values));
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 
     }
     render(){
