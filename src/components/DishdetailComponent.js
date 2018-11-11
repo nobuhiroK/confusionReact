47a1d6070ab99e7e809b0a3bf1bd7e8b035104a2
nobuhiroK <nobuhk@uw.edu>
2018-11-11 15:39:31 +0900

    Redux, Client-Server Communication and Fetch fixed bugs

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
+Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
